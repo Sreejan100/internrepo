1047bbf388dfee4367c0957927141c63c6e3afd0
Sreejan100 <chattopadhyaysreejan100@gmail.com>
2021-05-15 11:42:31 +0530

    second commit

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ mongoose.connect("mongodb+srv://admin:abir1000@cluster0.agmkp.mongodb.net/quiz")
 
 app.use("/",require("./routes/questionRoute"));
 
+
+if(process.env.NODE_ENV === 'production'){
+     app.use(express.static('quizapp/build'));
+     app.get("*",(req,res) => {
+         res.sendFile(path.resolve(__dirname,'quizapp','build','index.html'));
+     })
+}
+
 app.listen(3001,function() {
     console.log("express server is running on port 3001");
 })
